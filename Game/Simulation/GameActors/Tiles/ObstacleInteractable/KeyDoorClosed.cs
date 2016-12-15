1cdd5211c4be658731a1ce8b866636d6cd53be44
Martin Milota <milotov@seznam.cz>
2016-12-15 17:22:55 +0100

    + New corrected TW version

---
+++ Game/Simulation/GameActors/Tiles/ObstacleInteractable/KeyDoorClosed.cs
@@ -0,0 +1,38 @@
+﻿using VRageMath;
+using World.Atlas;
+using World.Atlas.Layers;
+using World.GameActors.Tiles.OnGroundInteractable;
+using World.ToyWorldCore;
+
+namespace World.GameActors.Tiles.ObstacleInteractable
+{
+    public class KeyDoorClosed : DynamicTile, ISwitchableGameActor
+    {
+        public KeyDoorClosed(ITilesetTable tilesetTable, Vector2I position) : base(tilesetTable, position)
+        {
+        }
+
+        public KeyDoorClosed(int tileType, Vector2I position) : base(tileType, position)
+        {
+        }
+
+        public ISwitchableGameActor Switch(GameActorPosition gameActorPosition, IAtlas atlas, ITilesetTable table)
+        {
+            KeyDoorOpened openedDoor = new KeyDoorOpened(table, Position);
+            bool added = atlas.Add(new GameActorPosition(openedDoor, (Vector2)Position, LayerType.OnGroundInteractable));
+            if (!added) return this;
+            atlas.Remove(new GameActorPosition(this, (Vector2)Position, LayerType.ObstacleInteractable));
+            return openedDoor;
+        }
+
+        public ISwitchableGameActor SwitchOn(GameActorPosition gameActorPosition, IAtlas atlas, ITilesetTable table)
+        {
+            return Switch(gameActorPosition, atlas, table);
+        }
+
+        public ISwitchableGameActor SwitchOff(GameActorPosition gameActorPosition, IAtlas atlas, ITilesetTable table)
+        {
+            return this;
+        }
+    }
+}
---
+++ Game/Simulation/GameActors/Tiles/OnGroundInteractable/StepSwitch.cs
@@ -0,0 +1,63 @@
+﻿using VRageMath;
+using World.Atlas;
+using World.GameActors.GameObjects;
+using World.ToyWorldCore;
+
+namespace World.GameActors.Tiles.OnGroundInteractable
+{
+    public class StepSwitch : DynamicTile, ISwitcherGameActor, IDetectorTile, IAutoupdateableGameActor
+    {
+        public ISwitchableGameActor Switchable { get; set; }
+
+        public StepSwitch(ITilesetTable tilesetTable, Vector2I position)
+            : base(tilesetTable, position)
+        {
+        }
+
+        public StepSwitch(int tileType, Vector2I position)
+            : base(tileType, position)
+        {
+        }
+
+        public void Switch(GameActorPosition gameActorPosition, IAtlas atlas, ITilesetTable table)
+        {
+            Switchable = Switchable?.SwitchOn(null, atlas, table);
+        }
+
+        public bool RequiresCenterOfObject => false;
+
+
+        private bool m_wasActive = false;
+        private bool m_isActive = false;
+        public void ObjectDetected(IGameObject gameObject, IAtlas atlas, ITilesetTable tilesetTable)
+        {
+            if (m_wasActive)
+            {
+                m_isActive = true;
+                return;
+            }
+            m_isActive = true;
+            NextUpdateAfter = 1;
+            atlas.RegisterToAutoupdate(this);
+        }
+
+        public int NextUpdateAfter { get; private set; } = 0;
+
+        private const int DELAY = 8;
+
+        public void Update(IAtlas atlas, ITilesetTable table)
+        {
+            if (m_isActive)
+            {
+                Switchable?.SwitchOn(null, atlas, table);
+                m_isActive = false;
+                m_wasActive = true;
+                NextUpdateAfter = DELAY;
+                return;
+            }
+            Switchable?.SwitchOff(null, atlas, table);
+            NextUpdateAfter = 0;
+            m_wasActive = false;
+        }
+    }
+}
